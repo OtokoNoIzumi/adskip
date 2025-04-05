998c33b534b011c0a9067331ef240f033794c5fb
Izumi.Qu <29002083+OtokoNoIzumi@users.noreply.github.com>
2025-04-06 07:43:36 +0800

    增加字符串支持

--- start_api.bat
+++ start_api.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d %~dp0
+chcp 65001
 call %USERPROFILE%\anaconda3\Scripts\activate.bat solara-develop
 echo 正在启动广告检测API服务，请保持此窗口打开...
 python api_server.py --ssl-keyfile=izumihostpab.life.key --ssl-certfile=izumihostpab.life.pem
